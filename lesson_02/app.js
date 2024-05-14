3f97b70aa28d1635dc0becfa4d2986a31ab5fb8e
DharshanMP <shand3649@gmail.com>
2024-05-14 14:58:29 +0530

    modified

--- lesson_02/app.js
+++ lesson_02/app.js
@@ -1,1 +1,28 @@
-let names = 'Dharshan';
+// 1st project 
+
+// let naming = 'My name is :' + 'Dharshan'
+
+// let coffee = 5
+// let bagel = 3 
+// let total_cost = coffee + bagel 
+// alert(`Items (${1+1}) \t Total cost : $${total_cost}`);
+
+// 2nd project
+
+// let coffee = 599
+// let bagel = 295
+// total_cost = (coffee + bagel)/100
+// alert(`Total cost: $${total_cost} \nThank you, come again!`)
+
+
+// 3rd project
+
+let basketballs = 2095
+let shipping_charge = 499
+let T_shirt = 799
+let Total_item_cost = ((2095*2)+(799*2))/100
+let Total_shipping = (499*2)/100
+let Total_before_tax = Total_item_cost+Total_shipping
+let tax = 6.79
+
+console.log(`Items (${1+1+1+1}):            $${Total_item_cost}\nShipping & handling:  $${Total_shipping}\nTotal before tax:     $${Total_before_tax}\nEstimated tax(10%):   $${6.79}\n\nOrder total:            $${Total_before_tax+tax}`);
